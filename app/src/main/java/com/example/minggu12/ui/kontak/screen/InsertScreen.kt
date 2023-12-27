007d76f1287292e79ec453de5c4a34437274da87
Teuku Muhammad Attar Fadhilah <attarku05@gmail.com>
2023-12-27 22:35:17 +0700

    Add FormInputSiswa function

--- app/src/main/java/com/example/minggu12/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/minggu12/ui/kontak/screen/InsertScreen.kt
@@ -1,2 +1,17 @@
 package com.example.minggu12.ui.kontak.screen
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.minggu12.ui.kontak.viewmodel.InsertUiEvent
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputSiswa(
+    insertUiEvent: InsertUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertUiEvent) -> Unit = {},
+    enabled: Boolean = true
+) {
+
+}
