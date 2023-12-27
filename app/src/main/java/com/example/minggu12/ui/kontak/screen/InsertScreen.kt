cb844a50f3a5de53e663e7556171a798576b0e39
Teuku Muhammad Attar Fadhilah <attarku05@gmail.com>
2023-12-27 22:59:44 +0700

    Add EntryKontakScreen function

--- app/src/main/java/com/example/minggu12/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/minggu12/ui/kontak/screen/InsertScreen.kt
@@ -4,19 +4,31 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.minggu12.navigation.DestinasiNavigasi
+import com.example.minggu12.ui.PenyediaViewModel
+import com.example.minggu12.ui.TopAppBarKontak
 import com.example.minggu12.ui.kontak.viewmodel.InsertUiEvent
 import com.example.minggu12.ui.kontak.viewmodel.InsertUiState
+import com.example.minggu12.ui.kontak.viewmodel.InsertViewModel
+import kotlinx.coroutines.launch
 import org.w3c.dom.Text
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -58,7 +70,7 @@ fun FormInputSiswa(
             enabled = enabled,
             singleLine = true
         )
-        
+
         if (enabled) {
             Text(
                 text = "Isi semua data",
@@ -87,7 +99,7 @@ fun EntryKontakBody(
             insertUiEvent = insertUiState.insertUiEvent,
             onValueChange = onSiswaValueChange,
             modifier = Modifier.fillMaxWidth()
-            )
+        )
         Button(
             onClick = onSaveClick,
             shape = MaterialTheme.shapes.small,
@@ -95,5 +107,47 @@ fun EntryKontakBody(
         ) {
             Text(text = "Simpan")
         }
+    }
+}
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Siswa"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryKontakScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBarKontak(
+                title = DestinasiEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        EntryKontakBody(
+            insertUiState = viewModel.insertKontakState,
+            onSiswaValueChange = viewModel::updateInsertKontakState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertKontak()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
     }
 }
